6d60b256c9637f939ad4f444b5ed5d703c694083
CaireMM007 <retoditt@gmail.com>
2025-06-11 20:25:01 -0300

    Add files via upload

---
+++ GitHub/exersice1.py
@@ -0,0 +1,5 @@
+name = input("Hola, ingresa tu nombre de usuario: ")
+age = int(input("Ingresa tu edad: "))
+height = float(input("Y su altura: "))
+
+print(f"Hola {name}, tienes {age} años y mides {height} metros.")
